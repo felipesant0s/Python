1db501eb27338b264dfb8c7dc62991fcb66c7e96
felipesant0s <felipesant0stux@gmail.com>
2015-07-17 21:10:46 -0300

    csv2mysql

--- PythonMySQL/csv2mysql.py
+++ PythonMySQL/csv2mysql.py
@@ -1,15 +1,19 @@
 #!/usr/bin/python
 import mysql.connector
 import csv
+import os
+import commands
 
-db = mysql.connector.connect(host="localhost", port="3306", user="root", passwd="root", database="teste")
+os.system("ssh comissionamento 'cd $SAGE ; ./geracsv.sh'")
+
+db = mysql.connector.connect(host="localhost", port="3306", user="root", passwd="root", database="sage")
 cursor = db.cursor()
-insert_data = "REPLACE INTO digitais (pto, id, descricao, estado) VALUES (%s, %s, %s, %s)"
+insert_sage = "REPLACE INTO digitais (pto, id, descricao, estado, qualidade) VALUES (%s, %s, %s, %s, %s)"
 
 with open('/media/CODIGOS/Python/PythonMySQL/sage.csv', 'rb') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
-    for data in csv_reader:
-        cursor.execute(insert_data, data)
+    for sage in csv_reader:
+        cursor.execute(insert_sage, sage)
         db.commit()
 
 cursor.close()
