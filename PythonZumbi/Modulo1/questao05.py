6ae776c6b790eab0aade5314c56d32f0c6713a41
felipesant0s <felipesant0stux@gmail.com>
2015-07-23 17:27:07 -0300

    Scripts curso python para Zumbis

--- PythonZumbi/Modulo1/questao05.py
+++ PythonZumbi/Modulo1/questao05.py
@@ -5,5 +5,4 @@ velocidade = float(input("Digite a velocidade media: "))
 tempo = distancia/velocidade
 min = (round(tempo % 1, 2))
 min = (min * 60)
-
 print('O tempo esperado da viagem e de {:.0f} horas e {:.0f} minutos'.format(tempo,min))
